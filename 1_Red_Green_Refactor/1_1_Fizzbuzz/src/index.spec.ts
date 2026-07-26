a6be2643984fd8070d98f1de3e2468db658bb727
Moali-learning <mohammedshokai99@outlook.com>
2026-07-26 14:50:05 -0400

    red

--- 1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,8 @@
 
 describe("fizzbuzz", () => {
-
+it('should return string when we pass a number', () => {
+    const fizzbuzz = new FizzBuzz();
+    expect(fizzbuzz.calculate(1)).toEqual(expect.any(String))
+})
 
 });
