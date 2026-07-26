6b36aee88d1cb5e714bc1b433feb02c5ef620ac1
Moali-learning <mohammedshokai99@outlook.com>
2026-07-26 15:23:16 -0400

    refactor tests 1

--- 1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,16 +1,17 @@
 import { FizzBuzz } from './fizzbuzz';
 describe("fizzbuzz", () => {
+    let fizzbuzz: FizzBuzz
+    beforeEach(() => {
+        fizzbuzz = new FizzBuzz();
+    })
 it('should return "Fizz" when passing multiple of 3', () => {
-    const fizzbuzz = new FizzBuzz();
     expect(fizzbuzz.calculate(3)).toBe("Fizz")
 })
 it('should return "Buzz" when passing multiple of 5', () => {
-     const fizzbuzz = new FizzBuzz();
     expect(fizzbuzz.calculate(5)).toBe("Buzz")
 
 })
 it('should return "FizzBuzz" when passing multiple of 15', () => {
-     const fizzbuzz = new FizzBuzz();
     expect(fizzbuzz.calculate(15)).toBe("FizzBuzz")
 
 })
