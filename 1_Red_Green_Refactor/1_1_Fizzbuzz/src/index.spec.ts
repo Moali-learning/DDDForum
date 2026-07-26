58f2501dcf7c36d2215a63067abc17e3391936c1
Moali-learning <mohammedshokai99@outlook.com>
2026-07-26 15:17:42 -0400

    red 2 correct one

--- 1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -6,7 +6,7 @@ it('should return "Fizz" when passing multiple of 3', () => {
 })
 it('should return "Buzz" when passing multiple of 5', () => {
      const fizzbuzz = new FizzBuzz();
-    expect(fizzbuzz.calculate(3)).toBe("Buzz")
+    expect(fizzbuzz.calculate(5)).toBe("Buzz")
 
 })
 
