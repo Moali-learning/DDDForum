d194412de978d310d8156513bad24e44518b5ab1
Moali-learning <mohammedshokai99@outlook.com>
2026-07-26 15:20:34 -0400

    green 3

--- 1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -3,7 +3,9 @@ export class FizzBuzz {
     calculate(int: number): string {
        if (int == 3){
         return "Fizz";
+       } else if (int == 5){
+        return "Buzz";
        }
-       return "Buzz";
+       return "FizzBuzz";
     }
 }
