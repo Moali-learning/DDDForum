005d1f3acca5696732e1a6aebc66475fbc9ee0e5
Moali-learning <mohammedshokai99@outlook.com>
2026-07-27 06:10:26 -0400

    refactor

--- 1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,37 +4,23 @@ describe("fizzbuzz", () => {
     beforeEach(() => {
         fizzbuzz = new FizzBuzz();
     })
-it('should return "Fizz" when passing multiple of 3', () => {
-    expect(fizzbuzz.calculate(3)).toBe("Fizz")
+it.each([3, 9,42])('should return "Fizz" when passing multiple of 3', (n) => {
+    expect(fizzbuzz.calculate(n)).toBe("Fizz")
 })
 it('should return "Buzz" when passing multiple of 5', () => {
     expect(fizzbuzz.calculate(5)).toBe("Buzz")
 
 })
-it('should return "FizzBuzz" when passing multiple of both 3 and 5', () => {
-    expect(fizzbuzz.calculate(15)).toBe("FizzBuzz")
-
-})
-it('should return "Fizz" when passing multiple of 3', () => {
-    expect(fizzbuzz.calculate(9)).toBe("Fizz")
+it.each([15, 45])('should return "FizzBuzz" when passing multiple of both 3 and 5', (n) => {
+    expect(fizzbuzz.calculate(n)).toBe("FizzBuzz")
 
 })
 it('should return "43" when passing invalid multiple ', () => {
     expect(fizzbuzz.calculate(43)).toBe("43")
 
 })
-it('should return "Fizz" when passing multiple 3', () => {
-    expect(fizzbuzz.calculate(42)).toBe("Fizz")
-
-})
-it('should return "FizzBuzzz" when passing multiple both 3 and 5 ', () => {
-    expect(fizzbuzz.calculate(45)).toBe("FizzBuzz")
-})
-it('should return "" when passing invalid multiple ', () => {
+it.each([102,-12])('should return "" when passing invalid multiple ', () => {
     expect(fizzbuzz.calculate(102)).toBe("")
-})
-it('should return "" when passing invalid multiple ', () => {
-    expect(fizzbuzz.calculate(-12)).toBe("")
 })
 
 });
