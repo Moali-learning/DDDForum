13334bd781310065fac33675559feacb7403f6d4
Moali-learning <mohammedshokai99@outlook.com>
2026-08-02 12:50:13 -0400

    applied PR feedback

--- 1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -15,11 +15,11 @@ it.each([15, 45])('should return "FizzBuzz" when passing multiple of both 3 and 
     expect(fizzbuzz.calculate(n)).toBe("FizzBuzz")
 
 })
-it('should return "43" when passing invalid multiple ', () => {
+it('should return input values when passing invalid multiple ', () => {
     expect(fizzbuzz.calculate(43)).toBe("43")
 
 })
-it.each([102,-12])('should return "" when passing invalid multiple ', () => {
+it.each([102,-12])('should return "" when passing out of bounds values', () => {
     expect(fizzbuzz.calculate(102)).toBe("")
 })
 
