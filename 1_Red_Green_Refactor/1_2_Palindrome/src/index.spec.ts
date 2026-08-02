e7b330379182037ab4099f46775765bbd4fc6686
Moali-learning <mohammedshokai99@outlook.com>
2026-08-02 13:22:21 -0400

    ⚪️ REFACTOR

--- 1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,16 +1,17 @@
 import Palindrome from "./index"
 
 describe('palindrome checker', () => {
+    let palindrome: Palindrome;
+    beforeEach(() => {
+        palindrome = new Palindrome();
+    })
     it.each(["mom","wow"])("should return true when passing valid word", (word: string) => {
-        const palindrome = new Palindrome();
         expect(palindrome.isPalindrome(word)).toBeTruthy();
     })
     it("should return false whe passing 'bill'", () => {
-        const palindrome = new Palindrome();
         expect(palindrome.isPalindrome("bill")).toBeFalsy();
     })
     it("shold return false when passing Momx", () => {
-        const palindrome = new Palindrome();
         expect(palindrome.isPalindrome("Momx")).toBeFalsy();
     })
 })
