be0404f01109546c4957d6c525f16dcf294bf16c
Moali-learning <mohammedshokai99@outlook.com>
2026-08-02 13:02:59 -0400

    🔴RED

--- 1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,8 @@
+import Palindrome from "./index"
 
 describe('palindrome checker', () => {
-
+    it("should return true when passing 'mom'", () => {
+        const palindrome = new Palindrome();
+        expect(palindrome.isPalindrome("mom")).toBeTruthy();
+    })
 })
