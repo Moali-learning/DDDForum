2fbb2ae71cc91f9b46718a98a7bdfaf3dceab971
Moali-learning <mohammedshokai99@outlook.com>
2026-08-03 18:24:06 -0400

    Palindrome (#2)
    * 🔴RED
    
    * 🟢 GREEN
    
    * ⚪️ REFACTOR
    
    * 🔴RED
    
    * 🟢 GREEN
    
    * 🔴RED
    
    * 🟢 GREEN
    
    * ⚪️ REFACTOR
    
    * 🔴RED
    
    * 🟢 GREEN
    
    * 🔴RED
    
    * 🟢 GREEN

--- 1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,21 @@
+import Palindrome from "./index"
 
 describe('palindrome checker', () => {
+    let palindrome: Palindrome;
+    beforeEach(() => {
+        palindrome = new Palindrome();
+    })
+    it.each(["mom","wow", "MoM", "xMomx"])("should return true when passing valid word", (word: string) => {
+        expect(palindrome.isPalindrome(word)).toBeTruthy();
+    })
+    it.each(["bill", "Momx"])("should return false whe passing invalid words", (word: string) => {
+        expect(palindrome.isPalindrome(word)).toBeFalsy();
+    })
+    it.each(["Was It A Rat I Saw","Never Odd or Even", "1Never Odd or Even1"])("should return true when passing a valid sentence", (sentence: string) => {
+         expect(palindrome.isPalindrome(sentence)).toBeTruthy();
+    })
+    it("should returns false when passing an invalid sentence", () => {
+        expect(palindrome.isPalindrome("Never Odd or Even1")).toBeFalsy();
+    })
 
 })
---
+++ 1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -0,0 +1,8 @@
+export default class Palindrome {
+    isPalindrome(str: string):boolean {
+        const reverse = str.split("").reverse().join("").trim().replace(/\s/g, "").toLowerCase();
+        const original  = str.trim().replace(/\s/g, "").toLowerCase();
+        
+        return original == reverse;
+    }
+}
