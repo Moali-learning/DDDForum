20af16797de136a9e2e360bab2678712ecd0f54d
Moali-learning <mohammedshokai99@outlook.com>
2026-08-03 20:05:59 -0400

    🔴 Red

--- 2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -2,13 +2,18 @@
 import PasswordValidator from "./index"
 describe('password validator', () => {
 
-  test('should return false when passing invalid Password', () => {
+  it('should return false when passing "mom" ', () => {
 
     const resultObject = PasswordValidator.validate("mom")
     // assert
     expect(resultObject.result).toBeFalsy();
     expect(resultObject.errors![0].message).toContain("5 - 15");
 
+  })
+  it('should return true with valid password', () => {
+    const resultObject = PasswordValidator.validate("Mom12345")
+      expect(resultObject.result).toBeTruthy();
+      expect(resultObject.errors).toBeNull();
   })
 })
 
