5d11cf5873dc720f7ef5564f9c712d6042cabda2
Moali-learning <mohammedshokai99@outlook.com>
2026-08-03 20:03:05 -0400

    🟢 Green

--- 2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -7,6 +7,7 @@ describe('password validator', () => {
     const resultObject = PasswordValidator.validate("mom")
     // assert
     expect(resultObject.result).toBeFalsy();
+    expect(resultObject.errors![0].message).toContain("5 - 15");
 
   })
 })
--- 2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -10,6 +10,10 @@ interface Result{
 
 export default class PasswordValidator{
     static validate(str: string): Result{
-        return {result: true, errors: null}
+        return {result: false, errors:[
+            {type: "Invalid Range", message: "Should be at least 5 - 15 characters" },
+            {type: "Invalid Case", message: "Password should contain at least 1 upper case character"},
+            {type: "Digit Missing", message: "Password should contain at least 1 digit"},
+        ]}
     }
 }
